1a8d16072eb5148b9435c320e83ca36ed5375d1f
DESKTOP-GB3ECGF\Grzywna <grzywna997@gmail.com>
2020-10-20 19:46:55 +0200

    init.c -added comments describing assembly code

--- shared/init.c
+++ shared/init.c
@@ -10,7 +10,7 @@ extern int main(void);
 void my_init (void) __attribute__ ((naked)) __attribute__ ((section (".init1")));
 
 void my_init (void) {
-	// load SP, reset __zer_reg__
+	// clear __zero_reg__, load SP
 	asm volatile(
 	"clr	r1" "\n\t"
 	"out	0x3f, r1"	"\n\t"
@@ -20,17 +20,18 @@ void my_init (void) {
 	
 	// load .data (RAM) from flash 
 	asm volatile(
+	// load _etext, __data_start, __data_end
 	"mov	r30, %A0"	"\n\t"
 	"mov	r31, %B0"	"\n\t"
 	"mov	r26, %A1"	"\n\t"
 	"mov	r27, %B1"	"\n\t"
 	"mov	r24, %A2"	"\n\t"
 	"mov	r25, %B2"	"\n\t"
-	
+	// Compare __data_start __data_end (in case of no data)
 	"cp		r24, r26"	"\n\t"
 	"cpc	r25, r27"	"\n\t"
 	"breq	load_end"	"\n\t"
-	
+	// Load .data from flash to RAM
 	"load_loop:"		"\n\t" 
 	"lpm	r0, z+"		"\n\t"
 	"st		x+, r0"		"\n\t"
@@ -42,23 +43,25 @@ void my_init (void) {
 	: "r" (&_etext), "r" (&__data_start), "r" (&__data_end)
 	);
 	
-	// clear bss
+	// Clean RAM for uninitialized data (bss)
 	asm volatile(
+	// Load __bss_start, __bss_end
 	"mov	r26, %A0"	"\n\t"
 	"mov	r27, %B0"	"\n\t"
 	"mov	r24, %A1"	"\n\t"
 	"mov	r25, %B1"	"\n\t"
-	
+	// Compare __bss_start __bss_end (in case of no bss data)
 	"cp		r24, r26"	"\n\t"
 	"cpc	r25, r27"	"\n\t"
 	"breq	bss_end"	"\n\t"
-	
+	// Load "0" into RAM for lengh: __bss_end - __bss_start
 	"bss_loop:"			"\n\t"
 	"st		x+, r1"		"\n\t"
 	"cp		r24, r26"	"\n\t"
 	"cpc	r25, r27"	"\n\t"
 	"brne	bss_loop"	"\n\t"
 	"bss_end:"			"\n\t"
+	// Last init instruction - jump to main
 	"rjmp main"
 	:
 	: "r" (&__bss_start), "r" (&__bss_end)
